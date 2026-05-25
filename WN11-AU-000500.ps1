da44b5449cf8e938c5afdd0f383baccbf4720b6b
TheSunnyDev <109054640+TheSunnyDev@users.noreply.github.com>
2026-05-25 16:44:45 -0700

    Update WN11-AU-000500.ps1

--- WN11-AU-000500.ps1
+++ WN11-AU-000500.ps1
@@ -21,9 +21,8 @@
     PowerShell Ver. : 
 
 .USAGE
-    Put any usage instructions here.
     Example syntax:
-    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+    PS C:\> .\STIG-ID-WN10-AU-000500.ps1 
 #>
 
 $path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application"
