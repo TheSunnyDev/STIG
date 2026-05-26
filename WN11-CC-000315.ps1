b5c8fd2e642cb651e3d1b2fa17038fdffd19ecd5
TheSunnyDev <109054640+TheSunnyDev@users.noreply.github.com>
2026-05-25 19:24:05 -0700

    Update WN11-CC-000315.ps1

--- WN11-CC-000315.ps1
+++ WN11-CC-000315.ps1
@@ -15,12 +15,11 @@
     STIG-ID         : WN11-CC-000315
     Documentation   : https://stigaview.com/products/win11/v2r7/WN11-CC-000315/
 
-.USAGE
-    Example syntax:
+.EXAMPLE
     PS C:\> .\WN11-CC-000315.ps1
+    Note: Must be run in an elevated PowerShell console (Run as Administrator).
 #>
 
-#Run this code in elevated PowerShell console.
 #Create the path if it doesn't exist
 $path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Installer"
 If (!(Test-Path $path)) { New-Item -Path $path -Force }
