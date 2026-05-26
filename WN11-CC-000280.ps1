837a5d23626d279690147c6f1208c30c16214a87
TheSunnyDev <109054640+TheSunnyDev@users.noreply.github.com>
2026-05-25 18:38:24 -0700

    Create WN11-CC-000280.ps1

---
+++ WN11-CC-000280.ps1
@@ -0,0 +1,29 @@
+<#
+.SYNOPSIS
+    Enabling this setting ensures that policies will be reprocessed even if none have been changed, so any unauthorized changes are forced to match the domain-based group policy settings again.
+
+.NOTES
+    Author          : Sunny Cannon
+    LinkedIn        : linkedin.com/in/sunnycannon/
+    GitHub          : github.com/TheSunnyDev
+    Date Created    : 2026-05-25
+    Last Modified   : 2026-05-25
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000090
+    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-CC-000280/
+
+.USAGE
+    Example syntax:
+    PS C:\> .\WN11-CC-000280.ps1
+#>
+
+#Run this code in elevated PowerShell console.
+
+$path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
+If (!(Test-Path $path)) { New-Item -Path $path -Force }
+Set-ItemProperty -Path $path -Name "fPromptForPassword" -Type DWord -Value 1
+
+#Verify the script is successful. Expected Value is 1.
+Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Name fPromptForPassword
