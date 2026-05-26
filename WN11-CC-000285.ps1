f9bd54e826faaae45582118cbcf9fdcbd829c1cc
TheSunnyDev <109054640+TheSunnyDev@users.noreply.github.com>
2026-05-25 18:42:49 -0700

    Create WN11-CC-000285.ps1

---
+++ WN11-CC-000285.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+  Allowing unsecure RPC communication exposes the system to man-in-the-middle attacks and data disclosure attacks.
+
+.NOTES
+    Author          : Sunny Cannon
+    LinkedIn        : linkedin.com/in/sunnycannon/
+    GitHub          : github.com/TheSunnyDev
+    Date Created    : 2026-05-25
+    Last Modified   : 2026-05-25
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000285
+    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-CC-000285/
+
+.USAGE
+    Example syntax:
+    PS C:\> .\WN11-CC-000285.ps1
+#>
+
+#Run this code in elevated PowerShell console.
+$path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
+If (!(Test-Path $path)) { New-Item -Path $path -Force }
+Set-ItemProperty -Path $path -Name "fEncryptRPCTraffic" -Type DWord -Value 1
+
+#Verify the script is successful. Expected Value is 1.
+Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Name fEncryptRPCTraffic
